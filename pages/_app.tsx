bd089bf9978b736437efdfba54d906f89599cac8
junggri <gkgkgk6933@gmail.com>
2022-08-19 18:21:52 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import "../src/styles/index.scss"
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
